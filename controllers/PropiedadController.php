a30a2f694122096fe2cb840389e98e09c102d62f
rcg7 <racaga85@gmail.com>
2022-10-25 05:55:24 +0200

    Add

--- controllers/PropiedadController.php
+++ controllers/PropiedadController.php
@@ -3,12 +3,17 @@
 namespace Controllers;
 use MVC\Router;
 use Model\Propiedad;
+use Model\Vendedor;
+use Intervention\Image\ImageManagerStatic as Image;
+
 
 class PropiedadController {
     public static function index(Router $router) {
 
         $propiedades = Propiedad::all();
-        $resultado = null;
+
+        // Muestra mensaje condicional
+        $resultado = $_GET['resultado'] ?? null;
 
         $router->render('propiedades/admin', [
             'propiedades' => $propiedades,
@@ -18,9 +23,56 @@ class PropiedadController {
     public static function crear(Router $router) {
 
         $propiedad = new Propiedad;
+        $vendedores = Vendedor::all();
+
+        // Arreglo con mensajes de errores
+        $errores = Propiedad::getErrores();
+
+
+        if($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+            /** Crear una nueva instancia  */
+            $propiedad = new Propiedad($_POST['propiedad']);
+    
+    
+            /** Subida de archivos */ 
+    
+            // Generar un nombre único
+            $nombreImagen = md5( uniqid( rand(), true ) ) . ".jpg";
+    
+            // Setear la imagen
+            // Realiza un reslize a la imagen con intervention
+            if($_FILES['propiedad']['tmp_name']['imagen']) {
+            $image = Image::make($_FILES['propiedad']['tmp_name']['imagen'])->fit(800,600);
+            $propiedad->setImagen($nombreImagen);
+            }
+    
+            // Validar
+            $errores = $propiedad->validar();
+    
+            
+    
+    
+    
+            if(empty($errores)) {
+    
+            // Crear la carpeta para subir imagenes
+            if(!is_dir(CARPETA_IMAGENES)) {
+                mkdir(CARPETA_IMAGENES);
+            }
+    
+            // Guardar la imagen en el servidor
+            $image->save(CARPETA_IMAGENES . $nombreImagen);
+    
+            // Guarda en la base de datos
+            $propiedad->guardar();
+        }
+    }
        
         $router->render('propiedades/crear', [
-            'propiedad' => $propiedad
+            'propiedad' => $propiedad,
+            'vendedores' => $vendedores,
+            'errores' => $errores
         ]);
     }
     public static function actualizar() {
